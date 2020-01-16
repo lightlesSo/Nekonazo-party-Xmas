9f183a18cfec9ef28047f345725b598d6db905d6
wxyshr250@163.com <wxyshr250@163.com>
2020-01-16 14:49:00 +0800

    diff函数完成了，可以精确返回变化，以后可以根据这个对现有程序改造或者添加新功能

--- web-server/obs.rkt
+++ web-server/obs.rkt
@@ -1,41 +1,33 @@
 #lang racket
 (require "public-defines.rkt")
-#;(define (obs-hash hashtable fun proc)  ;大概要没用了  
-  "hashtable在fun执行后如果变化，就执行proc"
-  (define old-hash-code (equal-hash-code hashtable))
-  (begin0 (fun)
-         ; (display old-hash-code)
-          (if (equal? old-hash-code (equal-hash-code hashtable))
-              (void)
-              (proc))))
 (define (eq-hash-code-equal? hash1 hash2)
   (equal? (eq-hash-code hash1) (eq-hash-code hash2)))
 (define not-obs-keys (list 'drawsteps 'sema))
-(define (dif-hash old-hash new-hash)
-  (define (dif old new)
-    (define (dif-proc sub-old sub-new keys)
-      (if (null? keys)
-          #f ;每一次去掉一个kv相等的 最后为空就是全等， 不然直接返回new 这样是为了把比较过的去掉，减少重复
-          ;问题是比较的并不同，不一定是值或者hash 需要判断
-          (if (eq-hash-code-equal? (hash-ref sub-old (car keys))(hash-ref sub-new (car keys)))              
-              (dif-proc (hash-remove sub-old (car keys)) (hash-remove sub-new (car keys)) (cdr keys))
-              new)))
-    (if (and (equal? (hash-count old)(hash-count new)) (hash-keys-subset? old new))
-        (dif-proc old new (hash-keys old))
-        new))
 
-  (if (not (and (hash? new-hash) (hash? old-hash)))
-      (if (eq? new-hash old-hash)
-          '()
-          new-hash)
-      (let (( old-hash (hash-remove-list old-hash not-obs-keys))
-            ( new-hash (hash-remove-list new-hash not-obs-keys)))
-        (if (dif old-hash new-hash)
-            new-hash
-            (filter (lambda (x) (not (null? (cdr x))))
-                    (hash-map new-hash (lambda (k v)
-                                         (define ret (dif-hash (hash-ref old-hash k) (hash-ref new-hash k) ))
-                                         (cons k ret) )))))))
+(define (diff-hash old-hash new-hash #:add-sign (add '+) #:remove-sign (remove '-) #:modify-sign (modify '$))
+  (define (diff old new change)
+    (if (hash-empty? old)
+        (append (map (lambda (k) (cons add k)) (hash-keys new))  change)
+        (let ((oldK (hash-iterate-key old 0)))
+          (cond ((not (hash-has-key? new oldK)) (diff (hash-remove old oldK) new (cons (cons remove oldK) change)))
+                ((eq-hash-code-equal? (hash-ref old oldK) (hash-ref new oldK)) (diff (hash-remove old oldK) (hash-remove new oldK) change))
+                (#t (diff (hash-remove old oldK) (hash-remove new oldK) (cons (cons modify oldK) change)))))))
+  (define (proc key old-hash new-hash )
+    (if (not (and (hash? new-hash) (hash? old-hash)))
+        '()
+        (let* (( old-hash (hash-remove-list old-hash not-obs-keys))
+               ( new-hash (hash-remove-list new-hash not-obs-keys))
+               (change (diff old-hash new-hash '()))
+               (same (hash-remove-list old-hash (filter-map
+                                                 (lambda (c) (if (eq? '+ (car c))
+                                                                 #f
+                                                                 (cdr c) ))
+                                                 change))))
+          (cons key (append change (filter (lambda (x) (and (not (null?  x)) (not (null? (cdr x)))))
+                                           (hash-map same (lambda (k v)
+                                                            (proc k (hash-ref old-hash k) (hash-ref new-hash k) )
+                                                            ))))))))
+  (cdr (proc 'root old-hash new-hash)))
 (define (obs-hash get-hash fun proc)  
   " (get-hash) 因为是不可变 需要一个取得的方法
 hashtable在满足条件后执行后如果变化，就执行(proc old new diflist)"
@@ -43,27 +35,10 @@ hashtable在满足条件后执行后如果变化，就执行(proc old new diflis
  ; 如果用法信号多线程方法，需要自己独立一个状态，保存之前的，为什么输入法只能输入第一个词 现在的方法也可以把sleep当req过程，单独里线程
   (define old-hash (get-hash))
   (begin0 (fun)
-          (let ((dif-list (dif-hash old-hash (get-hash))))
+          (let ((dif-list (diff-hash old-hash (get-hash))))
             (if (null? dif-list)
                 (void)
                 (proc old-hash (get-hash) dif-list)))));不需要临时变量 大概 中途变了点问题也不大，而且这个步骤没有什么小号吧
-#;(define (broadcast-if-room-status-modify fun)
-  (define (broadcast)
-    (define rooms-namelist 
-      (filter string?
-              (hash-map (name-status) 
-                        (lambda (key value) 
-                          (if (equal? "rooms" (hash-ref value 'state '()))
-                              key
-                              '())))))
-    (define (oneroomstatus key value)
-      (make-immutable-hash (list (cons 'room key) 
-                       (cons 'peoplenum (length (hash-ref value 'names)))	  
-                       (cons 'roomstatus (hash-ref value 'gamestate)))))
-    (define roomsstatus (hash-map  (room-status) oneroomstatus))
-    ;(display rooms-namelist)
-    (broadcast-json rooms-namelist "room" "currentrooms" `#hasheq((roomlist . ,roomsstatus)) ))
-  (obs-hash (room-status) fun broadcast))
 (define (broadcast-room-status )
     (define rooms-namelist 
       (filter symbol?
